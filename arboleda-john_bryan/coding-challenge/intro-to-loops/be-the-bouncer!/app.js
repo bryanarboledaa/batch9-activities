737016d7b47bf61da136a11fda9b4e89a95b1dbe
bryanarboledaa <80822041+bryanarboledaa@users.noreply.github.com>
2021-05-12 11:13:29 +0800

    color-my-chessboard: done

---
+++ arboleda-john_bryan/coding-challenge/intro-to-loops/be-the-bouncer!/app.js
@@ -0,0 +1,41 @@
+const nightClubRegister = [
+    {
+        name: 'Tony',
+        lastname: 'Stark',
+        age: 25,
+        gender: 'male'
+    },
+    {
+        name: 'Harry',
+        lastname: 'Potter',
+        age: 16,
+        gender: 'male'
+    },
+    {
+        name: 'Hermione',
+        lastname: 'Granger',
+        age: 17,
+        gender: 'female'
+    },
+    {
+        name: 'Steve',
+        lastname: 'Rogers',
+        age: 62,
+        gender: 'male'
+    }
+]
+
+for (let i = 0; i < nightClubRegister.length; i++);
+    if(nightClubRegister[i] === 'male');
+        console.log('Hi Mr.' + nightClubRegister.name + 'thank you for coming!');
+
+    if (age < 18) {
+        console.log('Sorry! We cannot let you in because you are underage');
+    }
+    if (gender = 'male') {
+        console.log('Hi Mr.' + name + 'thank you for coming!');
+    }
+    if (gender = 'female') {
+        console.log('Hi Mrs./Ms.' + name + 'thank you for coming!');
+    }
+
